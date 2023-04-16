e39efbf741c99999c20be4c31f4932ec7634c7a6
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-17 04:24:45 +0430

    adding dynamic-inline-css-classes

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,10 +25,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
-        <input  style={{borderColor : !isValid ? 'red' : '#ccc' , background : !isValid ? 'salmon' : 'transparent'
-        }}  type="text" onChange={goalInputChangeHandler} />
+        <input  type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
